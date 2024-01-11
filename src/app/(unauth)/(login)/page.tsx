a6755cba658b7fd8f8ccd14ee2e929b19aa69bb7
christopher-estanis <christopher.estanis@gmail.com>
2024-01-11 08:41:51 -0300

    feature(login): add login stylesto css

--- src/app/(unauth)/(login)/page.tsx
+++ src/app/(unauth)/(login)/page.tsx
@@ -4,11 +4,11 @@ export default function Home() {
   return (
     <main className="h-full flex flex-col justify-center w-2/5 gap-y-6">
       <section className='flex flex-col gap-y-4'>
-        <h1 className="text-5xl font-semibold">Entrar.</h1>
-        <h2>Não possui uma conta? <a href='https://www.w3schools.com/html/html5_semantic_elements.asp'>Cadastre-se</a></h2>
+        <h1>Entrar.</h1>
+        <p>Não possui uma conta? <a href='https://www.w3schools.com/html/html5_semantic_elements.asp'>Cadastre-se</a></p>
       </section>
-      <input type="text" />
-      <input type="password" />
+      <input type="email" name="email" placeholder='@ E-Mail' />
+      <input type="password" name="password" placeholder='# Password' />
       <a href='https://www.w3schools.com/html/html5_semantic_elements.asp' className='self-end'>Esqueci a senha</a>
       <button type="submit" className='font-bold py-2.5 bg-gradient-to-r from-[#E54818] to-[#FF7F00]'>ENTRAR</button>
     </main>
